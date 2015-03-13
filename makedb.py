2b85b26815f1790e4b9d9f0cb6d3bf0a62f93b17
David Richfield <davidrichfield@gmail.com>
2015-03-14 00:46:00 +0100

    more database and project design

--- makedb.py
+++ makedb.py
@@ -3,13 +3,19 @@ con = sqlite3.connect('wishes.db')
 con.execute('''
 CREATE TABLE projectstatus (
   statusid INTEGER PRIMARY KEY,
-  name char(20) NOT NULL
+  name TEXT NOT NULL
   )
 ''')
 con.execute('''
 CREATE TABLE clientstatus (
   statusid INTEGER PRIMARY KEY,
-  name char(20) NOT NULL
+  name TEXT NOT NULL
+  )
+''')
+con.execute('''
+CREATE TABLE framestatus (
+  statusid INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
   )
 ''')
 con.execute('''
@@ -22,35 +28,57 @@ CREATE TABLE clients (
 con.execute('''
 CREATE TABLE frametypes (
   frametypeid INTEGER PRIMARY KEY,
-  ext char(10) NOT NULL,
-  name char(50) NOT NULL)
+  ext TEXT NOT NULL,
+  name TEXT NOT NULL)
+''')
+con.execute('''
+CREATE TABLE frames (
+  frameid INTEGER PRIMARY KEY,
+  status INTEGER NOT NULL,
+  wishid INTEGER NOT NULL,
+  clientid INTEGER,
+  filename TEXT
+  renderstart TEXT,
+  renderend TEXT,
+  uploadstart TEXT,
+  uploadend TEXT,
+  FOREIGN KEY(status) REFERENCES framestatus(statusid),
+  FOREIGN KEY(wishid) REFERENCES wishes(wishid),
+  FOREIGN KEY(clientid) REFERENCES clients(clientid))
 ''')
 con.execute('''
 CREATE TABLE engines (
   engineid INTEGER PRIMARY KEY,
-  name char(20) NOT NULL)
+  name TEXT NOT NULL)
 ''')
 con.execute('''
 CREATE TABLE wishes (
   wishid INTEGER PRIMARY KEY,
-  name char(100) NOT NULL,
+  name TEXT NOT NULL,
   majorversion INTEGER NOT NULL,
   minorversion INTEGER NOT NULL,
-  versionsuffix char(10),
+  versionsuffix TEXT,
   status INTEGER NOT NULL,
   frametype INTEGER NOT NULL,
+  firstframe INTEGER,
+  lastframe INTEGER,
   engine INTEGER NOT NULL,
   FOREIGN KEY(status) REFERENCES projectstatus(statusid),
   FOREIGN KEY(frametype) REFERENCES frametypes(frametypeid),
   FOREIGN KEY(engine) REFERENCES engines(engineid))
 ''')
 #Populate engines table
-con.execute("INSERT INTO engines (name) VALUES ('Internal')")
+con.execute("INSERT INTO engines (name) VALUES ('internal')")
 con.execute("INSERT INTO engines (name) VALUES ('cycles')")
 #Populate client status table
 con.execute("INSERT INTO clientstatus (name) VALUES ('new')")
 con.execute("INSERT INTO clientstatus (name) VALUES ('good')")
 con.execute("INSERT INTO clientstatus (name) VALUES ('bad')")
+#Populate frame status table
+con.execute("INSERT INTO framestatus (name) VALUES ('empty')")
+con.execute("INSERT INTO framestatus (name) VALUES ('draft')")
+con.execute("INSERT INTO framestatus (name) VALUES ('running')")
+con.execute("INSERT INTO framestatus (name) VALUES ('done')")
 #Populate project status table
 con.execute("INSERT INTO projectstatus (name) VALUES ('stopped')")
 con.execute("INSERT INTO projectstatus (name) VALUES ('running')")
