74b4d1fafd18c127f6afa0753f8800198a14ef1a
David Richfield <davidrichfield@gmail.com>
2015-03-16 20:27:18 +0100

    Add code to add a project

---
+++ wish_form.tpl
@@ -0,0 +1,21 @@
+%#template to generate an HTML table from a list of tuples (or list of lists, or tuple of tuples or ...)
+<h1>Make a Blender Wish</h1>
+<form action="/makewish" method="post">
+  Project name:           <input name="wish_name" type="text" /><br>
+  Render engine:          <select name="en">
+                            %for en in enginelist:
+                              <option value="{{en[0]}}">{{en[1]}}</option>
+                            %end #for
+                          </select><br>
+  Frame type:             <select name="ft">
+                            %for ft in frametypelist:
+                              <option value="{{ft[0]}}">{{ft[1]}}</option>
+                            %end #for
+                          </select><br>
+  Blender major version:  <input name="maj_ver" type="number" value="2"><br>
+  Blender minor version:  <input name="min_ver" type="number" value="73"><br>
+  Blender version suffix: <input name="ver_suf" type="text"><br>
+  First frame to render:  <input name="fr1" type="number" value="1"><br>
+  Last frame to render:   <input name="frn" type="number"><br>
+  <input value="MakeWish" type="submit" />
+</form>
--- wishserver.py
+++ wishserver.py
@@ -3,28 +3,6 @@ from bottle import Bottle, route, get, post, request, run, template, debug
 
 app = Bottle()
 
-@app.route('/list') 
-def list():
-  conn = sqlite3.connect('wishes.db')
-  c = conn.cursor()
-  c.execute('''
-  SELECT
-    wishes.name AS wish_name, 
-    projectstatus.name AS status_name,
-    frametypes.name AS frametype_name,
-    engines.name AS engine_name
-  FROM 
-    wishes 
-    LEFT JOIN projectstatus ON wishes.status = projectstatus.statusid
-    LEFT JOIN frametypes ON wishes.frametype = frametypes.frametypeid
-    LEFT JOIN engines ON wishes.engine = engines.engineid
-  ''')
-  result = c.fetchall()
-  result = [(u'Project name', u'Status', u'Frame type', u'Engine')] + result
-  c.close()
-  output = template('make_table', rows=result)
-  return output
-
 def showproj(wishid):
   conn = sqlite3.connect('wishes.db')
   c = conn.cursor()
@@ -50,6 +28,62 @@ def showproj(wishid):
     output = template('proj_detail', rows=result)
   return output
   
+@app.route('/list') 
+def list():
+  conn = sqlite3.connect('wishes.db')
+  c = conn.cursor()
+  c.execute('''
+  SELECT
+    wishes.name AS wish_name, 
+    projectstatus.name AS status_name,
+    frametypes.name AS frametype_name,
+    engines.name AS engine_name
+  FROM 
+    wishes 
+    LEFT JOIN projectstatus ON wishes.status = projectstatus.statusid
+    LEFT JOIN frametypes ON wishes.frametype = frametypes.frametypeid
+    LEFT JOIN engines ON wishes.engine = engines.engineid
+  ''')
+  result = c.fetchall()
+  result = [(u'Project name', u'Status', u'Frame type', u'Engine')] + result
+  c.close()
+  output = template('make_table', rows=result)
+  return output
+
+@app.get('/makewish') #Create a new project: get action
+def makewish():
+  #Get the list of allowed frametypes to insert into the template.
+  conn = sqlite3.connect('wishes.db')
+  c = conn.cursor()
+  c.execute("SELECT frametypeid, name FROM frametypes")
+  frametypelist = c.fetchall()
+  c.execute("SELECT engineid, name FROM engines")
+  enginelist = c.fetchall()
+  c.close()
+  wishform = template('wish_form', enginelist=enginelist, frametypelist=frametypelist) #Generate a form with pre-populated option lists.
+  return wishform
+
+@app.post('/makewish') #Create a new project: post action
+def do_makewish():
+  pn=request.forms.get('wish_name')
+  ft=request.forms.get('ft')
+  en=request.forms.get('en')
+  maj_ver=request.forms.get('maj_ver')
+  min_ver=request.forms.get('min_ver')
+  ver_suf=request.forms.get('ver_suf')
+  fr1=request.forms.get('fr1')
+  frn=request.forms.get('frn')
+  conn = sqlite3.connect('wishes.db')
+  c = conn.cursor()
+  c.execute('''
+  INSERT INTO wishes(name, majorversion, minorversion, versionsuffix, status, frametype, firstframe, lastframe, engine)
+  VALUES 
+    (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  ''', (pn, maj_ver, min_ver, ver_suf, 1, ft, fr1, frn, en))
+  new_wish_id = c.lastrowid
+  conn.commit()
+  c.close()
+  return showproj(new_wish_id)
 
 @app.route('/wish/<wishid:int>') #Return the project by number - showproj will redirect if not valid ID.
 def showprojbynum(wishid):
